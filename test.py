117354cc7af881196dbe31c176e92b453bde1344
michael-poon <michael-poon@users.noreply.github.com>
2025-07-27 08:32:24 +0000

    deploy: 410a1655e3743f8a2884595cc4ea0264dc3ab3fc

--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.aastocks.com/tc/stocks/analysis/stock-aafn/00001/0/hk-stock-news/1")
-
-input("請按 Enter 關閉瀏覽器...")
-driver.quit()
